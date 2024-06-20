79973f917c8f0c1e157945f647d6768bf361e833
Shivaraj Patil <patilshivaraj65@gmail.com>
2024-06-20 11:18:12 +0530

    login-form-done

--- libs/user/src/lib/login/login.component.ts
+++ libs/user/src/lib/login/login.component.ts
@@ -1,19 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'org-login',
-  standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  standalone: true,                                                             
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   search = new FormControl('');
+  loginForm: FormGroup = this.fb.group({});
 
-  loginForm= new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''), 
-  })
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]], // Email field with required and email validators
+      password: ['', [Validators.required,Validators.minLength(8),Validators.maxLength(16),]] // Password field with required and minimum length validators
+    });
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+  }
 }
+
+
